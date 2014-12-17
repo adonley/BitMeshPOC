f4f44e1edfb3bc9b74f844b8d528105d3fafde0a
Andrew Donley <andrewjdonley@gmail.com>
2014-12-17 01:48:40 -0800

    Added saving keys to disk. Working on server connection. Next step is data transfer.

--- src/pairclient.py
+++ src/pairclient.py
@@ -2,15 +2,22 @@ import zmq
 import random
 import sys
 import time
+import pairserver
 
 port = "5556"
 context = zmq.Context()
-socket = context.socket(zmq.PAIR)
+socket = context.socket(zmq.REP)
 socket.connect("tcp://localhost:%s" % port)
 
 while True:
-    msg = socket.recv()
-    print msg
-    socket.send("client message to server1")
-    socket.send("client message to server2")
-    time.sleep(1)
+	socket.send(client_start_connection)
+	time.sleep(1)
+	msg = socket.recv()
+	print msg
+
+
+
+    #if(msg == server_start_connection):
+    #	print msg
+    #socket.send("client message to server2")
+
--- src/pairserver.py
+++ src/pairserver.py
@@ -5,11 +5,29 @@ import time
 
 port = "5556"
 context = zmq.Context()
-socket = context.socket(zmq.PAIR)
+socket = context.socket(zmq.REP)
 socket.bind("tcp://*:%s" % port)
 
+# start connection phrease
+client_start_connection = 'HI'
+
+# start connection response
+server_start_connection = "HELLO"
+
+# end connection phrase
+end_connection = 'END'
+
 while True:
-    socket.send("Server message to client3")
+    
     msg = socket.recv()
     print msg
-    time.sleep(1)
+
+    # handle connection states
+    if(msg == client_start_connection):
+    	time.sleep(.5)
+    	socket.send(server_start_connection)
+    elif(msg == end_connection):
+    	socket.sent(end_connection)
+
+    print msg
+
